190e2a901e9e66128ea5b4f65eb56d7e10b9c6af
thomasRalee <thomas.leera@gmail.com>
2024-05-13 12:44:31 +0100

    chore: add debug logs for uploadImages script

--- ts-scripts/uploadImages.ts
+++ ts-scripts/uploadImages.ts
@@ -1,11 +1,17 @@
-import 'dotenv/config'
 import axios from 'axios'
+import { config } from 'dotenv'
 import * as FormData from 'form-data'
 import { readdirSync, createReadStream } from 'node:fs'
 import { readJSONFile, updateJSONFile } from './helper/utils'
 
+config({ path: './../.env' })
 const extensions = ['png', 'jpg', 'jpeg', 'svg', 'webp']
 const imgDirectoryPath = './images'
+
+console.log({
+  CLOUD_FLARE_API_KEY: process.env.CLOUD_FLARE_API_KEY,
+  CLOUD_FLARE_ACCOUNT_ID: process.env.CLOUD_FLARE_ACCOUNT_ID
+})
 
 const uploadImage = async (imageName: string) => {
   try {
