bb1ba3aaacb8a298e9c1cceca73b7d2eeeb78147
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2024-06-11 20:40:34 +0200

    chore: lint

--- ts-scripts/helper/execShellCommand.ts
+++ ts-scripts/helper/execShellCommand.ts
@@ -1,12 +1,13 @@
 export function execShellCommand(cmd: any) {
-  console.log("Executing " + cmd);
-  const exec = require('child_process').exec;
-  return new Promise((resolve, reject) => {
+  const exec = require('child_process').exec
+
+  return new Promise((resolve) => {
     exec(cmd, (error: any, stdout: any, stderr: any) => {
-       if (error) {
-        console.warn(error);
-       }
-    resolve(stdout? stdout : stderr);
-    });
-  });
+      if (error) {
+        console.warn(error)
+      }
+
+      resolve(stdout ? stdout : stderr)
+    })
+  })
 }
