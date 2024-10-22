906611411bd64588626ff92c5151ac80d299d174
thomasRalee <thomas.leera@gmail.com>
2024-10-22 12:16:27 +0800

    chore: folder restructure tokens file add backward compatability - skip deploy

--- ts-scripts/generateTokens.ts
+++ ts-scripts/generateTokens.ts
@@ -123,6 +123,8 @@ export const generateTokensList = async (network: Network) => {
     sortedList
   )
 
+  await updateJSONFile(`tokens/${getNetworkFileName(network)}.json`, sortedList)
+
   console.log(`✅✅✅ GenerateTokens ${network}`)
 }
 
