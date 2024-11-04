b122953cabd6cc2a908ec8068be84e87a76d07f6
ivan-angjelkoski <ivanangelkoski95@gmail.com>
2024-11-04 16:15:20 +0100

    chore: pgt inj-usdt sc

--- ts-scripts/data/grid/derivative.ts
+++ ts-scripts/data/grid/derivative.ts
@@ -1,4 +1,14 @@
-export const mainnetGridMarkets = []
+export const mainnetGridMarkets = [
+  {
+    slug: 'inj-usdt-perp',
+    contractAddress: 'inj1hcsytnq2salprkkscr73fzj7y9p8srgpncjhqx'
+  }
+]
 export const stagingGridMarkets = []
-export const testnetGridMarkets = []
+export const testnetGridMarkets = [
+  {
+    slug: 'inj-usdt-perp',
+    contractAddress: 'inj1vlcry9phmky442w44f3kkkn5sqyhn987yjer6n'
+  }
+]
 export const devnetGridMarkets = []
