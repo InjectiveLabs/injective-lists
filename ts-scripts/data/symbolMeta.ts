ba5775e1c070203bcabf9c2137919fe737643654
PedroInj <pedroinjective@gmail.com>
2025-02-20 12:06:03 +0100

    Adding $Pedro Token

--- ts-scripts/data/symbolMeta.ts
+++ ts-scripts/data/symbolMeta.ts
@@ -2157,5 +2157,13 @@ export const symbolMeta: Record<string, TokenSymbolMeta> = {
     name: 'JNI',
     logo: 'jni.svg',
     coinGeckoId: ''
-  }
+  },
+  
+  PEDRO : {
+    decimals: 18,
+    symbol: 'PEDRO',
+    name: 'PEDRO',
+    logo: 'Pedro.svg',
+    coinGeckoId: ''
+  },
 }
