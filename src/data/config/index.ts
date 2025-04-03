553a8bebcc275ba50a2b76b3b5a1bb9ff81e3694
thomasRalee <thomas.leera@gmail.com>
2025-04-03 18:32:08 +0800

    feat: setup chain upgrade config

---
+++ src/data/config/index.ts
@@ -0,0 +1,7 @@
+export const chainUpgradeConfig = {
+  proposalId: 515,
+  blockHeight: 112636306,
+  disableMaintenance: false,
+  proposalMsg:
+    'Scheduled maintenance on January 11th, 2024 at ~14:00 UTC to implement the Injective Kishan mainnet upgrade.'
+}
