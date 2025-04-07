87fba273f2754041a80b75c98e6c55d0a975551c
ThomasRalee <ThomasRalee@users.noreply.github.com>
2025-04-07 20:53:46 +0000

    chore: generate json files - skip deploy

--- src/data/config/index.ts
+++ src/data/config/index.ts
@@ -1,1 +1,11 @@
-export const chainUpgradeConfig = {}
+import { ChainConfig } from './../../types'
+
+const template = {
+  proposalId: 515,
+  blockHeight: 111949209,
+  disableMaintenance: false,
+  proposalMsg:
+    'Scheduled maintenance on January 11th, 2024 at ~14:00 UTC to implement the Injective Volan mainnet upgrade.'
+} as ChainConfig
+
+export const chainUpgradeConfig = {} as ChainConfig | {}
