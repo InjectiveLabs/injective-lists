331ed873cbdcf65dc996194fd30bd8ec5cd467e6
thomasRalee <thomas.leera@gmail.com>
2025-04-30 18:57:05 +0200

    chore: remove lyora chain upgrade config

--- src/data/config/index.ts
+++ src/data/config/index.ts
@@ -1,17 +1,11 @@
 import { ChainConfig } from './../../types'
 
-const template = {
-  proposalId: 515,
-  blockHeight: 111949209,
-  disableMaintenance: false,
-  proposalMsg:
-    'Scheduled maintenance on January 11th, 2024 at ~14:00 UTC to implement the Injective Volan mainnet upgrade.'
-} as ChainConfig
-
-export const chainUpgradeConfig = {
+const LyoraMainnetUpgrade = {
   proposalId: 518,
   blockHeight: 114590000,
   disableMaintenance: true,
   proposalMsg:
     'Scheduled maintenance on April 22nd, 2025 at ~13:30 UTC to implement the Injective Lyora mainnet upgrade.'
-} as ChainConfig | {}
+} as ChainConfig
+
+export const chainUpgradeConfig = {} as ChainConfig | {}
