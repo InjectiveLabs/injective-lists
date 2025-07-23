c4e079d2c01cbfcd27e45c8f8bf724ddcefc6b39
thomasRalee <thomas.leera@gmail.com>
2025-07-24 02:20:27 +0800

    chore: add wrappedINJ in evm

--- src/data/tokens/evm.ts
+++ src/data/tokens/evm.ts
@@ -5,15 +5,21 @@ export const devnetTokens = []
 export const testnetTokens = [
   {
     ...symbolMeta.INJ,
-    symbol: 'wINJ',
-    name: 'Wrapped INJ',
+    symbol: 'DINJ',
+    name: 'Demo INJ',
     address: 'erc20:0xe1c64DDE0A990ac2435B05DCdac869a17fE06Bd2'
   },
   {
     ...symbolMeta.USDT,
-    symbol: 'wUSDT',
-    name: 'Tether',
+    symbol: 'DUSDT',
+    name: 'Demo Tether',
     address: 'erc20:0x719fF496ddf37C56F2A958676f630f417a4084aa'
+  },
+  {
+    ...symbolMeta.INJ,
+    symbol: 'WINJ',
+    name: 'Wrapped INJ',
+    address: 'erc20:0x0000000088827d2d103ee2d9A6b781773AE03FfB'
   }
 ]
 
