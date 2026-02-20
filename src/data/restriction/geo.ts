643657065ba7e9e850b20227f7c6f107052473ec
thomasRalee <thomas.leera@gmail.com>
2026-02-20 14:18:48 +0800

    chore: update restricted country list

--- src/data/restriction/geo.ts
+++ src/data/restriction/geo.ts
@@ -1,1 +1,34 @@
-export const restrictedCountries: string[] = ['CU', 'IR', 'KP', 'SY']
+export const restrictedCountries: string[] = [
+  'KP',
+  'IR',
+  'CU',
+  'SY',
+  'RU',
+  'AF',
+  'MM',
+  'VE',
+  'YE',
+  'LY',
+  'SD',
+  'SS',
+  'SO',
+  'IQ',
+  'LB',
+  'CF',
+  'CD',
+  'ER',
+  'ML',
+  'GW',
+  'BI',
+  'ZW',
+  'UA'
+]
+
+// Replace UA when we have support for region-based restrictions
+export const restrictedRegions: string[] = [
+  'UA-43', // Crimea
+  'UA-14', // Donetsk
+  'UA-09', // Luhansk
+  'UA-65', // Kherson
+  'UA-23' // Zaporizhzhia
+]
